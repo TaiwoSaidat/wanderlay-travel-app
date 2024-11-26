e8c317316413f7e56bc10fc09797b5adcf0aafe0
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-11-26 17:19:31 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-teal-500 font-bold">
+        hellow helloe world Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Temporibus, doloribus similique velit deserunt blanditiis voluptas
+        quis placeat sunt amet adipisci nemo cum expedita tempore. Possimus
+        vitae dolorum distinctio porro consequuntur? Suscipit excepturi, natus
+        libero sint vero optio, voluptates distinctio ab labore, est explicabo
+        ducimus quia! Hic eveniet qui facere velit obcaecati sed voluptatum a
+        nisi commodi iste, recusandae aspernatur eos.
+      </h1>
+    </>
+  );
+}
